0f072bc7a35ce3ddc330e23d1ee9a80bdafef32a
Marcos Campos <marcos.b.campos14@gmail.com>
2024-03-08 16:48:31 -0300

    feat(view): Resume page impl

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,8 +1,29 @@
+<script setup lang="ts">
+import CV from "../assets/files/curriculo_marcos.pdf"
+</script>
 <template>
   <main>
-    <h1>CV</h1>
+    <h1 id="title">Curriculum Vitae</h1>
+    <div id="container">
+    <embed :src="CV" id="pdf"></embed>
+    </div>
   </main>
 </template>
 <style scoped>
+
+#container {
+  margin: 0 15em;
+  height: 80em;
+}
+#title {
+  color: var(--color-heading);
+  text-align: center;
+  padding: 1em 0;
+}
+
+#pdf {
+  width: 100%;
+  height: 100%;
+}
 
 </style>
